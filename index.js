db93cf0fef15a8350c5e19bba109e1a755d29b8a
Murat ÇAY <murtcay@gmail.com>
2021-10-12 16:24:40 +0300

    first get request response

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    const blog = { id: 1, title: "Blog title", description: "Blog description" };
+    res.send(blog);
+})
+
+app.listen(port, () => {
+    console.log(`Server is listening on ${port}...`);
+})
