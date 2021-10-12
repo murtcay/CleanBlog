954f4c2daa432ab05411530bb405856501156778
Murat ÇAY <murtcay@gmail.com>
2021-10-12 23:32:57 +0300

    add new post, store them in DB, then display them on home page

--- index.js
+++ index.js
@@ -1,29 +1,48 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const ejs = require('ejs');
 const app = express();
+const Post = require('./modules/Post');
+
 const port = 3000;
+
+// connect DB
+mongoose.connect('mongodb://localhost/cleanblog-test-db', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
 
 // TEMPALATE ENGINE
 app.set('view engine', 'ejs');
 
 // MIDDLEWARES
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // ROUTES
-app.get('/', (req, res) => {
-    res.render('index');
+app.get('/', async (req, res) => {
+    const posts = await Post.find({});
+    res.render('index', {
+        posts: posts
+    });
 });
 
 app.get('/about', (req, res) => {
-    res.render('about');
+  res.render('about');
 });
 
-app.get('/add_post', (req, res) => {
-    res.render('add_post');
+app.get('/add-post', (req, res) => {
+  res.render('add-post');
+});
+
+app.post('/add-post', async (req, res) => {
+  await Post.create(req.body);
+  res.redirect('/');
 });
 
 app.get('/post', (req, res) => {
-    res.render('post');
+  res.render('post');
 });
 
 app.listen(port, () => {
