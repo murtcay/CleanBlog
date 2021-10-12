94c80bdcfa58244eaf8f98666da09fc1616d91be
Murat ÇAY <murtcay@gmail.com>
2021-10-12 17:59:09 +0300

    routing and template rendering

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
 const express = require('express');
+const ejs = require('ejs');
 const app = express();
 const port = 3000;
 
+// TEMPALATE ENGINE
+app.set('view engine', 'ejs');
+
+// MIDDLEWARES
+app.use(express.static('public'));
+
+// ROUTES
 app.get('/', (req, res) => {
-    const blog = { id: 1, title: "Blog title", description: "Blog description" };
-    res.send(blog);
-})
+    res.render('index');
+});
+
+app.get('/about', (req, res) => {
+    res.render('about');
+});
+
+app.get('/add_post', (req, res) => {
+    res.render('add_post');
+});
+
+app.get('/post', (req, res) => {
+    res.render('post');
+});
 
 app.listen(port, () => {
-    console.log(`Server is listening on ${port}...`);
-})
+  console.log(`Server is listening on ${port}...`);
+});
